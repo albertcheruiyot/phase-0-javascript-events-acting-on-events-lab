ad87ab7aea73ddeb1cb6d146b0f5630b01653f7d
Albert Cheruiyot <bettalbert99@gmail.com>
2024-04-04 20:31:17 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "100px";
+
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+
+document.addEventListener("keyup", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  left += 10;
+
+  dodger.style.left = `${left}px`;
+}
+
+
+
